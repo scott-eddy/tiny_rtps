4e3d4b1fbe363b84453aa485ec8e641e210e0598
Eddy Scott <escott@morse-corp.com>
2017-08-31 17:05:24 -0400

    After much analysis with clang-tidy it appears that it is not particularly better than gcc with -Wall -Werr for C static code analysis.  Perhaps it is worth while looking into SonarQube
    This reverts commit 826f4de83957b01bf4c1356ce9aed8aabcd5ee01.

--- tests/rtps/participant_creation_tests.cpp
+++ tests/rtps/participant_creation_tests.cpp
@@ -3,19 +3,6 @@
 #include <gtest/gtest.h>
 #include "participant.h"
 
-TEST(ParticipantTesting, ParticipantCreation){
-  // Arrange
-  ParticipantFactory_t* participant_factory = ParticipantFactoryInit();
-  ParticipantAttributes_t attributes;
-
-  //Act
-  Participant_t* participant = participant_factory->CreateParticipant(participant_factory, &attributes);
-  // Assert
-  ASSERT_TRUE((participant != NULL));
-
-  free(participant);
-}
-
 TEST(ParticipantTesting, FactoryCreationDestruction){
   // Act
   ParticipantFactory_t* participant_factory = ParticipantFactoryInit();
@@ -27,5 +14,5 @@ TEST(ParticipantTesting, FactoryCreationDestruction){
 int main(int argc, char **argv){
   ::testing::InitGoogleTest(&argc, argv);
   int ret = RUN_ALL_TESTS();
-  return ret;
+  return 0;
 }
